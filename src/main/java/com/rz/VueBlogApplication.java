52024ab4e80175c1deb3a606b9165a348088b4e3
likefr <likefr@qq.com>
2021-09-09 20:40:38 +0800

    Initial commit

---
+++ src/main/java/com/rz/VueBlogApplication.java
@@ -0,0 +1,13 @@
+package com.rz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rz/VueBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
