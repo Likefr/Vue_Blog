f240855afbd16ab39e65ffcb5893d316e978c732
Likefr <likefr@qq.com>
2021-09-10 11:22:52 +0800

    测试远程

--- src/main/java/com/rz/controller/testController.java
+++ src/main/java/com/rz/controller/testController.java
@@ -1,8 +1,25 @@
 package com.rz.controller;
 
+import com.rz.util.JsonResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @version 1.0 2021/9/10
  * @outhor Likefr
  */
+@RestController
 public class testController {
+
+    @GetMapping("/test")    /*http://localhost:8080/test*/
+    public JsonResult test() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "第一个标题");
+        map.put("date", new Date().toLocaleString());
+        return JsonResult.success(map);
+    }
 }
---
+++ src/main/java/com/rz/util/JsonResult.java
@@ -0,0 +1,56 @@
+package com.rz.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @version 1.0
+ * @date 2021/09/09
+ * @outhor Likefr
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JsonResult implements Serializable {
+
+    private int code;
+    private String msg;
+    private Object data;
+
+    /*
+     * code 状态码
+     * msg 返回消息
+     * data json数据
+     */
+    public static JsonResult success(Object data) {
+        return success(200, "成功", data);
+    }
+
+    public static JsonResult success(int code, String msg, Object data) {
+        JsonResult result = new JsonResult();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+
+    public static JsonResult err(String msg) {
+        return err(400, msg, null);
+    }
+
+    public static JsonResult err(String msg, Object data) {
+        return err(400, msg, data);
+    }
+
+    public static JsonResult err(int code, String msg, Object data) {
+        JsonResult result = new JsonResult();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+}
